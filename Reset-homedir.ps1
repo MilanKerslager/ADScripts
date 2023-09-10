27f2bb8e68a2db04e07727e314637713351e3d75
Milan Keršláger <kerslager@eso-cl.cz>
2023-09-10 22:04:20 +0200

    fix HomeDrive (colon must be present)

--- Reset-homedir.ps1
+++ Reset-homedir.ps1
@@ -1,18 +1,8 @@
 ﻿Import-Module ActiveDirectory
 
-# reset HomeDirectory pro uživatele v určitém kontextu
-# pozor - pokud je kontext SearchBase neplatný, provede se pro všechny uživatele v AD
-
-$users = Get-ADUser -Filter * -SearchBase "OU=2023,OU=Žáci,OU=Eso-cl Users Office365 sync,DC=eso-cl,DC=cz"
-Reset-Homedir -Users $users -Share '\\srvfs01\Home$\'
-
-exit
-
-$users = Get-ADUser -Filter * -SearchBase "OU=Učitelé,OU=Eso-cl Users Office365 sync,DC=eso-cl,DC=cz"
-Reset-Homedir -Users $users -Share '\\srvfs01\Home_U$\'
-
-$users = Get-ADUser -Filter * -SearchBase "OU=Vedení,OU=Eso-cl Users Office365 sync,DC=eso-cl,DC=cz"
-Reset-Homedir -Users $users -Share '\\srvfs01\Home_U$\'
+# reset HomeDirectory for users in specific context (OU)
+# WARNING: if OU SearchBase is incorrect, it iterate all users in AD
+# WARNING: HomeDrive must have colon added after a letter
 
 function Reset-Homedir([System.Object] $Users, [string] $Share) {
     $count = $Users.Count
@@ -21,7 +11,23 @@ function Reset-Homedir([System.Object] $Users, [string] $Share) {
         $homeDirectory = $Share + $_.SamAccountName;
         Write-Host $count $_.SamAccountName $homeDirectory
         # Assign user's home directory path
-        Set-ADUser -Identity $_.SamAccountName -HomeDirectory $homeDirectory -HomeDrive H
+        Set-ADUser -Identity $_.SamAccountName -HomeDirectory $homeDirectory -HomeDrive H:
         $count--
     }
 }
+
+#$users = Get-ADUser -Filter * -SearchBase "OU=2023,OU=Žáci,OU=Eso-cl Users Office365 sync,DC=eso-cl,DC=cz"
+$users = Get-ADUser -Filter * -SearchBase "OU=Žáci,OU=Eso-cl Users Office365 sync,DC=eso-cl,DC=cz"
+Reset-Homedir -Users $users -Share '\\srvfs01\Home$\'
+
+#exit
+
+$users = Get-ADUser -Filter * -SearchBase "OU=Vedení,OU=Eso-cl Users Office365 sync,DC=eso-cl,DC=cz"
+Reset-Homedir -Users $users -Share '\\srvfs01\Home_U$\'
+
+#exit
+
+$users = Get-ADUser -Filter * -SearchBase "OU=Učitelé,OU=Eso-cl Users Office365 sync,DC=eso-cl,DC=cz"
+Reset-Homedir -Users $users -Share '\\srvfs01\Home_U$\'
+
+#exit
